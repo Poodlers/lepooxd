56b9f1438baaa8af3657e7ead652e4cf2073a811
Joana Mesquita <joanateixeiramesquita30@gmail.com>
2021-02-17 10:24:20 +0000

    Added string for choosing operation

--- Operations.java
+++ Operations.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class Operations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String operation = scanner.nextLine();
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
-        System.out.println(a + b);
+        if (operation.equals("mul")){
+            System.out.println(a * b);
+        }
+        else{
+            System.out.println(a + b);
+        }
+
         scanner.close();
     }
 }
